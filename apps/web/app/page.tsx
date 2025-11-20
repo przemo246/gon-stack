9bfec0a0691fbea90263c42a82390e3fa81f3b7b
polubis <polubis@wp.pl>
2025-11-20 07:24:44 +0100

    chore(repo): configure commitlint, husky, check updates and quality scripts

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,29 +1,29 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Gradient } from "@repo/ui/gradient";
-import { TurborepoLogo } from "@repo/ui/turborepo-logo";
+import Image from 'next/image';
+import { Card } from '@repo/ui/card';
+import { Gradient } from '@repo/ui/gradient';
+import { TurborepoLogo } from '@repo/ui/turborepo-logo';
 
 const LINKS = [
   {
-    title: "Docs",
-    href: "https://turborepo.com/docs",
-    description: "Find in-depth information about Turborepo features and API.",
+    title: 'Docs',
+    href: 'https://turborepo.com/docs',
+    description: 'Find in-depth information about Turborepo features and API.',
   },
   {
-    title: "Learn",
-    href: "https://turborepo.com/docs/handbook",
-    description: "Learn more about monorepos with our handbook.",
+    title: 'Learn',
+    href: 'https://turborepo.com/docs/handbook',
+    description: 'Learn more about monorepos with our handbook.',
   },
   {
-    title: "Templates",
-    href: "https://turborepo.com/docs/getting-started/from-example",
-    description: "Choose from over 15 examples and deploy with a single click.",
+    title: 'Templates',
+    href: 'https://turborepo.com/docs/getting-started/from-example',
+    description: 'Choose from over 15 examples and deploy with a single click.',
   },
   {
-    title: "Deploy",
-    href: "https://vercel.com/new",
+    title: 'Deploy',
+    href: 'https://vercel.com/new',
     description:
-      "Instantly deploy your Turborepo to a shareable URL with Vercel.",
+      'Instantly deploy your Turborepo to a shareable URL with Vercel.',
   },
 ];
 
@@ -42,7 +42,7 @@ export default function Page() {
             rel="noopener noreferrer"
             target="_blank"
           >
-            By{" "}
+            By{' '}
             <Image
               alt="Vercel Logo"
               className="dark:invert"
