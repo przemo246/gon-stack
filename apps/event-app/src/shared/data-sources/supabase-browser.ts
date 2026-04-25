920d67f9ece7009f954b99f19d0a529402e180c0
Przemek <przemo246@gmail.com>
2026-04-25 13:46:06 +0200

    refactor(event-app): rename and export supabase browser client

--- apps/event-app/src/shared/data-sources/supabase-browser.ts
+++ apps/event-app/src/shared/data-sources/supabase-browser.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr';
 import { type Database } from './db-schema';
 
-const supabase = createBrowserClient<Database>(
+export const supabaseBrowser = createBrowserClient<Database>(
   import.meta.env.PUBLIC_SUPABASE_URL,
   import.meta.env.PUBLIC_SUPABASE_PUBLISHABLE_KEY,
 );
